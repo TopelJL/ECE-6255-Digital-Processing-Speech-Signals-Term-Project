0980877860b56e55e04e7f6be8cc7018184f39e4
mritz17 <159719631+mritz17@users.noreply.github.com>
2026-03-24 19:39:08 -0400

    Update load_audio_file.m
    update code comments

--- load_audio_file.m
+++ load_audio_file.m
@@ -1,15 +1,22 @@
 function [x, fs, fileName] = load_audio_file(filePath)
-%LOAD_AUDIO_FILE Read WAV, mono, remove DC, peak-normalize to [-1, 1].
+% load wav file, convert to mono, remove dc, and normalize
 
+    % read audio file
     [x, fs] = audioread(filePath);
+
+    % get file name with extension
     [~, name, ext] = fileparts(filePath);
     fileName = [name ext];
 
+    % convert to mono if stereo
     if size(x, 2) > 1
         x = mean(x, 2);
     end
 
+    % remove dc offset
     x = x - mean(x);
+
+    % normalize signal to [-1, 1]
     peakVal = max(abs(x));
     if peakVal > 0
         x = x / peakVal;
